94b8bafc06455ac56bd2b2414c38226924fa44bb
hdphuc <hdphuc94@gmail.com>
2023-02-21 15:34:53 +0700

    add database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,17 +15,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email');
-            $table->string('password');
-            $table->string('login_type');
-            $table->text('asscess_token');
-            $table->string('expires_token');
-            $table->text('id_token');
-            $table->string('user_id');
-            $table->string('display_name');
-            $table->text('status_message');
-            $table->text('picture_url');
+            $table->string('username')->nullable();
+            $table->string('email')->nullable();
+            $table->string('password')->nullable();
+            $table->string('login_type')->nullable();
+            $table->text('asscess_token')->nullable();
+            $table->string('expires_token')->nullable();
+            $table->text('id_token')->nullable();
+            $table->string('user_id')->nullable();
+            $table->string('display_name')->nullable();
+            $table->text('status_message')->nullable();
+            $table->text('picture_url')->nullable();
             $table->timestamp('verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
