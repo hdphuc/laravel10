ae65513e274a0c46c41a709466d213d19ee48c8e
hdphuc <hdphuc94@gmail.com>
2023-02-23 15:36:04 +0700

    fix create post

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -28,7 +28,7 @@ class PostController extends Controller
     public function create()
     {
         $categories = Category::all();
-        return view('admin.posts.edit', compact('categories'));
+        return view('admin.posts.create', compact('categories'));
     }
 
     public function store(StorePostRequest $request)
